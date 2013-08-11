47f0b66784a3b6e9538b5214b8ffc35ab4ee0698
jameh <jamie.alban@gmail.com>
2013-08-11 17:31:30 -0400

    Move stuff around in setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 setup(
-    name = "HelloWorld",
+    name = "Hello-World",
     version = "0.1",
-    # scripts = [],
-    install_requires = ['flask==0.10.1'],
-
-    # metadata for upload to PyPI
-    author = "Me",
+    author = "My Name",
     author_email = "me@example.com",
-    description = "This is an Example Package",
-    license = "PSF",
-    keywords = "hello world example examples",
-    url = "http://example.com/HelloWorld/",   # project home page, if any
+    description = "Awesome python h4cks",
+    license = "Public Domain",
+
+    url = "example.com",
+
+    py_modules = [],
+    install_requires = [],
+
+    keywords = "",
 
     # could also include long_description, download_url, classifiers, etc.
 )
